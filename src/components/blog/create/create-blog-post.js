f9476ad78afee74d03bdd60a2808836dc7e23fdc
Aristotle The Axolotl <ttab1998@gmail.com>
2019-09-11 09:09:02 +0100

    seperated out create functionality

---
+++ src/components/blog/create/create-blog-post.js
@@ -0,0 +1,28 @@
+import { html, css, LitElement } from "lit-element";
+import './create-post-body.js';
+
+// TODO: post has a body, comment section, upvote/downvote,
+export default class CreatePost extends LitElement {
+  static get properties(){
+    return {
+
+    }
+  }
+
+  render() {
+    return html`
+      <post-body>
+        <span slot="mainBodyText">Enter your post text here...</span>
+      </post-body>
+    `;
+  }
+
+  static get styles() {
+    return css`
+
+    `;
+  }
+
+}
+
+customElements.define('create-blog-post', CreatePost);
