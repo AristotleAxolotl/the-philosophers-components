4b1cbf1e5efdf77b113898de559b7abadc761ce2
Aristotle The Axolotl <ttab1998@gmail.com>
2019-10-12 17:39:27 +0100

    test(nav-card): added custom behaviour tests

--- test/ui/components/tcp-navigation/tcp-nav-card/tcp-nav-card.test.js
+++ test/ui/components/tcp-navigation/tcp-nav-card/tcp-nav-card.test.js
@@ -27,4 +27,63 @@ describe('<nav-card>', () => {
       `);
     });
   });
+  describe('custom behaviour', () => {
+    let el;
+    beforeEach(async () => {
+      NavCard.register();
+      el = await fixture('<nav-card name="cusom"></nav-card>');
+    });
+    it('should allow a name to be set', async () => {
+      expect(el.name).to.equal('cusom');
+    });
+  });
+  describe('type of card: single', async () => {
+    let el;
+    beforeEach(async () => {
+      NavCard.register();
+      el = await fixture('<nav-card name="custom" resourceLinks=["/customLink"]></nav-card>');
+    });
+    it('should render as a normal card', async () => {
+      expect(el).shadowDom.to.equal(`
+      <div nav-card="">
+        <a href="/customLink">
+          custom
+        </a>
+      </div>
+      `);
+    });
+  });
+  describe('type of card: dropdown', async () => {
+    let el;
+    beforeEach(async () => {
+      NavCard.register();
+      el = await fixture(
+        `<nav-card name="custom" resourceLinks='["/customLink", "/customLinkTwo"]'></nav-card>`,
+      );
+    });
+    it('should render as a dropdown card', async () => {
+      expect(el).shadowDom.to.equal(`
+      <div nav-card="">
+        <div dropdown="">
+          <button dropbtn="">
+            custom
+            <i
+              arrow=""
+              down=""
+            >
+            </i>
+          </button>
+          <div dropdown-content="">
+            <a href="/customLink">
+              /customLink
+            </a>
+            <a href="/customLinkTwo">
+              /customLinkTwo
+            </a>
+          </div>
+        </div>
+      </div>
+      `);
+    });
+  });
 });
