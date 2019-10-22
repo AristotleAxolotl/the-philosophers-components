783ae84bd0bf1e36de12e2e63a68e72940bf8dc4
Aristotle The Axolotl <ttab1998@gmail.com>
2019-10-22 15:35:35 +0100

    fix(haunted): delete phantom folder

--- src/components/tcp-navigation/tcp-nav-bar/index.js
+++ src/components/tcp-navigation/tcp-nav-bar/index.js
@@ -4,7 +4,8 @@ import { NavCard } from '../tcp-nav-card';
 import { utils } from '../../lib';
 
 // TODO: something is going wrong between this and nav-card; array of links is not passing through properly & CSS not displaying properly
-
+// Think thats sorted now?
+// TODO: navBar should switch based on selection
 export class NavBar extends LitElement {
   static get properties() {
     return {
