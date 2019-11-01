32ebc9b782db65d6c84b5ea98515c137f95ee555
Aristotle The Axolotl <ttab1998@gmail.com>
2019-11-01 13:16:00 +0000

    chore(version): udpate version number

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { NavBar, NavCard } from './components/navigation';
 import { Post, PostBody, PostComments, PostUtilitiesBar } from './components/blog/display';
 import { CreatePost, CreatePostBody } from './components/blog/create';
 import { CreatePhilosophersTags, PhilosophersTags } from './components/tags';
+import { InfiniteScroller } from './components/infinite-scroller';
+import { Card } from './components/card';
 
 import * as lib from './components/lib';
 
@@ -20,5 +22,7 @@ export {
   CreatePostBody,
   CreatePhilosophersTags,
   PhilosophersTags,
+  InfiniteScroller,
+  Card,
   lib,
 };
