fbe1a7df2a50778ea1b43900596f8657cd064c91
Aristotle The Axolotl <ttab1998@gmail.com>
2020-01-28 12:44:05 +0000

    chore(linter): making so linting work/runs on commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,14 @@ module.exports = {
   rules: {
     ...sonarJsConfig,
     'no-only-tests/no-only-tests': 'error',
-    'class-methods-use-this': ['error', { exceptMethods: ['render', 'emits'] }],
+    'class-methods-use-this': ['error', { exceptMethods: ['render', 'emits','renderHeader','renderPage'] }],
+    'max-lines': ['warn', { max: 200 }],
+    'complexity': ['error', { max: 10 }],
+    'max-nested-callbacks': ['error', { max: 2 }],
+    'max-depth': ['error', { max: 2 }],
+    'max-params': ['error', { max: 3}],
+    'import/no-extraneous-dependencies': ['off'],
+    'import/extensions': ['off'],
+    'lit/no-invalid-html': ['off']
   },
 };
