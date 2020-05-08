0ee45b15d4461744800ef7741c8eef54b8a4e493
Aristotle The Axolotl <ttab1998@gmail.com>
2020-05-08 11:50:10 +0100

    chore(exports): fix exports

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import { PhilosophersHeader } from './components/header';
 import { ImageCss, ImageHtml } from './components/image';
 import { InfiniteScroller } from './components/infinite-scroller';
 import { NavBar, NavCard } from './components/navigation';
+import { PhilosophersPara } from './components/para';
 import { CreatePhilosophersTags, PhilosophersTags } from './components/tags';
 
 import * as lib from './components/lib';
@@ -28,6 +29,7 @@ export {
   PostComments,
   PostUtilitiesBar,
   PhilosophersHeader,
+  PhilosophersPara,
   TopBanner,
   CreatePost,
   CreatePostBody,
