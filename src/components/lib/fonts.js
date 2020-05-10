10820d5a38d2c2cd25edfaac1007bdda3525f090
Aristotle The Axolotl <ttab1998@gmail.com>
2020-05-10 10:13:04 +0100

    feat(fonts): added fonts

---
+++ src/components/lib/fonts.js
@@ -0,0 +1,29 @@
+import { css } from 'lit-element';
+
+export const fonts = {
+  Chich: css`
+    @font-face {
+      font-family: 'Century Gothic', CenturyGothic, AppleGothic, sans-serif;
+      font-display: swap;
+    }
+  `,
+  Progressive: css`
+    @font-face {
+      font-family: 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic,
+        sans-serif;
+      font-display: swap;
+    }
+  `,
+  Strong: css`
+    @font-face {
+      font-family: Futura, 'Trebuchet MS', Arial, sans-serif;
+      font-display: swap;
+    }
+  `,
+  Clean: css`
+    @font-face {
+      font-family: Calibri, Candara, Segoe, 'Segoe UI', Optima, Arial, sans-serif;
+      font-display: swap;
+    }
+  `,
+};
--- src/components/lib/index.js
+++ src/components/lib/index.js
@@ -3,5 +3,6 @@ import { mix } from './mix.js';
 import { pluggable } from './pluggable.js';
 import { emitsEvents } from './emitsEvents.js';
 import { utils } from './utils.js';
+import { fonts } from './fonts.js';
 
-export { mix, pluggable, emitsEvents, utils };
+export { mix, pluggable, emitsEvents, fonts, utils };
